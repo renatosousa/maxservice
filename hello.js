ce1d0bab728ec8bb8f8ca033a91965a6bf3fae73
renatosousa <natocosta@gmail.com>
2015-05-22 16:34:31 -0300

    hello.js

---
+++ hello.js
@@ -0,0 +1,19 @@
+var http = require('http');
+ var url = require('url');
+   
+  var server = http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>Dados da query string</h1>");
+    var result = url.parse(request.url, true);
+    for(var key in result.query){
+     if(key == "teste"){
+		response.write("<h2>"+result.query[key]+"</h2>");
+	 } else {
+		response.write("<h2>"+ key + "Não é um parâmetro válido" + "</h2>");
+	 }
+   } 
+   response.end();
+ });
+ server.listen(80, function(){
+   console.log('Servidor http.');
+});
