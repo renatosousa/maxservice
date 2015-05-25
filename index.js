57f83a8c9a2023ce3ce864f9ef66e2c2443f1b90
renatosousa <natocosta@gmail.com>
2015-05-25 16:48:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var http = require('http');
- var url = require('url');
-   
-  var server = http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Dados da query string</h1>");
-    var result = url.parse(request.url, true);
-    for(var key in result.query){
-     if(key == "teste"){
-		response.write("<h2>"+result.query[key]+"</h2>");
-	 } else {
-		response.write("<h2>"+ key + "Não é um parâmetro válido" + "</h2>");
-	 }
-   } 
-   response.end();
- });
- server.listen("3000", function(){
-   console.log('Servidor http.');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
